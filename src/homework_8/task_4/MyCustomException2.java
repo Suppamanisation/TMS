eb348b4512465c1f6881992595813f1d70656036
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-02 09:55:19 +0300

    Update MyCustomException2.java

--- src/homework_8/task_4/MyCustomException2.java
+++ src/homework_8/task_4/MyCustomException2.java
@@ -1,5 +1,6 @@
 package homework_8.task_4;
 
+// MyCustomException2 - плохое название класса
 public class MyCustomException2 extends Exception{
     public MyCustomException2(String message) {
         super(message);
