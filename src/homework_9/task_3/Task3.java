e34c98828f059f1b61b09a6c103335477e4f0b72
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-10 15:30:16 +0300

    Update Task3.java

--- src/homework_9/task_3/Task3.java
+++ src/homework_9/task_3/Task3.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 
 //Task 3
 //Прочитать файл из Task_2 и вывести на экран содержимое
+// +
 public class Task3 {
     private static final String FILE_PATH = "src/homework_9/task_2/task2";
 
