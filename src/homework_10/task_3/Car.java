a889eed067ea3938030d55e4928ca031d077da3a
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-13 22:23:41 +0300

    Update Car.java

--- src/homework_10/task_3/Car.java
+++ src/homework_10/task_3/Car.java
@@ -3,9 +3,9 @@ package homework_10.task_3;
 import java.awt.*;
 
 public abstract class Car {
-    long vin;
-    short year;
-    Color color;
+    long vin; // protected
+    short year; // protected
+    Color color; // protected
 
     public Car(long vin, short year, Color color) {
         this.vin = vin;
