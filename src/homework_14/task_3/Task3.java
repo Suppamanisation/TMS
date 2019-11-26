765fadd34226161039947e3d7fd1248544bd8e34
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-26 23:18:09 +0300

    Update Task3.java

--- src/homework_14/task_3/Task3.java
+++ src/homework_14/task_3/Task3.java
@@ -12,9 +12,12 @@ public class Task3 {
     public static void main(String[] args) {
         HashMap<String, List<Integer>> hashMap1 = new HashMap<>();
         hashMap1.put("array1", new ArrayList<>(Arrays.asList(1, 4, 5, 9)));
+       
         HashMap<String, List<Integer>> hashMap2 = new HashMap<>();
         hashMap2.put("array2", new ArrayList<>(Arrays.asList(2, 3, 6, 7, 8)));
+        
         hashMap2.forEach((key, value) -> hashMap1.merge(key, value, (list1, list2) -> {
+            // в идеале создать третий лист и туда два запихнуть
             list1.addAll(list2);
             return list1;
         }));
